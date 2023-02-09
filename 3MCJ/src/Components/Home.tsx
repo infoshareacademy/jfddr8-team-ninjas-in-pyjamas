0a1dc62258f766e0777dd995432e6e62cf9c6c12
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-02-09 14:12:20 +0100

    Add Input defaultValue

--- 3MCJ/src/Components/Home.tsx
+++ 3MCJ/src/Components/Home.tsx
@@ -3,16 +3,22 @@ import { globalContext } from "../Context/Context";
 import { useContext, useState } from "react";
 import { signOut } from "firebase/auth";
 import { firebaseAuth } from "../main";
-import SellerPage from "./SellerPage";
 import SellersList from "./SellersList";
 
 function Home() {
   const navigate = useNavigate();
-  const { setIsLogged, isLogged, searchingLocation, setSearchingLocation, searchingCategory, setSearchingCategory } = useContext(globalContext);
-  const [locationValue, setLocationValue] = useState<string>('')
-  const [categoryValue, setCategoryValue] = useState<string>('')
+  const {
+    setIsLogged,
+    isLogged,
+    searchingLocation,
+    setSearchingLocation,
+    searchingCategory,
+    setSearchingCategory,
+  } = useContext(globalContext);
+  const [locationValue, setLocationValue] = useState<string>("");
+  const [categoryValue, setCategoryValue] = useState<string>("");
 
-  const handleLogInOut = () => { 
+  const handleLogInOut = () => {
     if (!isLogged) {
       navigate("/login");
       setIsLogged(!isLogged);
@@ -27,12 +33,11 @@ function Home() {
     navigate("/registration");
   };
 
-  const handleSubmit = (e:React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     setSearchingLocation(locationValue);
     setSearchingCategory(categoryValue);
-    
-  }
+  };
 
   return (
     <div>
@@ -42,32 +47,51 @@ function Home() {
         </button>
         <button onClick={handleRegistration}>Zarejestruj się</button>
       </div>
-      <form onSubmit={(e)=>handleSubmit(e)}>
-      <label htmlFor="location">Lokalizacja:</label>
-      <select onChange={(e)=>{setLocationValue(e.target.value)}} name="location" id="location">
-        <option value="Wszystkie">Wszystkie</option>
-        <option value="Gdynia">Gdynia</option>
-        <option value="Sopot">Sopot</option>
-        <option value="Gdańsk">Gdańsk</option>
-      </select>
-      <label htmlFor="products">Kategoria produktu:</label>
-      <select onChange={(e)=>{setCategoryValue(e.target.value)}} name="products" id="products">
-        <option value="Wyroby Mięsne">Wyroby Mięsne</option>
-        <option value="Wyroby Rybne">Wyroby Rybne</option>
-        <option value="Wyroby Cukiernicze">Wyroby Cukiernicze</option>
-        <option value="Nabiał">Nabiał</option>
-        <option value="Wyroby Garmażeryjne">Wyroby Garmażeryjne</option>
-      </select>
-      <input type="submit" value={"Znajdź pyszności"} />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="location">Lokalizacja:</label>
+        <select 
+          defaultValue={"Wybierz miasto"}
+          onChange={(e) => {
+            setLocationValue(e.target.value);
+          }}
+          name="location"
+          id="location"
+        >
+          <option disabled >
+            Wybierz miasto
+          </option>
+          <option value="Gdynia">Gdynia</option>
+          <option value="Sopot">Sopot</option>
+          <option value="Gdańsk">Gdańsk</option>
+        </select>
+        <label htmlFor="products">Kategoria produktu:</label>
+        <select
+        defaultValue={"Wybierz produkt"}
+          onChange={(e) => {
+            setCategoryValue(e.target.value);
+          }}
+          name="products"
+          id="products"
+        >
+           <option disabled >
+            Wybierz produkt
+          </option>
+          <option value="Wyroby Mięsne">Wyroby Mięsne</option>
+          <option value="Wyroby Rybne">Wyroby Rybne</option>
+          <option value="Wyroby Cukiernicze">Wyroby Cukiernicze</option>
+          <option value="Nabiał">Nabiał</option>
+          <option value="Wyroby Garmażeryjne">Wyroby Garmażeryjne</option>
+        </select>
+        <input type="submit" value={"Znajdź pyszności"} />
       </form>
       <div>
-        <div>Wyroby Mięsne</div>
+        <div >Wyroby Mięsne</div>
         <div>Wyroby Rybne</div>
         <div>Wyroby Cuiernicze</div>
         <div>Nabiał</div>
         <div>Wyroby Garmażeryjne</div>
       </div>
-      <SellersList/>
+      <SellersList />
     </div>
   );
 }
