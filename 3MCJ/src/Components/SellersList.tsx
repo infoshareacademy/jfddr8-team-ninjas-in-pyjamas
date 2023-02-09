cd08d764776c492ef0a30289a16cfac649627bea
Rafał Konieczny <rkonieczny87@gmail.com>
2023-02-09 21:09:44 +0100

    filter funkcion works
    Co-authored-by: Katarzyna Sęk <Kasiatko@users.noreply.github.com>
    Co-authored-by: Agnieszka Szczepańska <agnieszka-szczepanska@users.noreply.github.com>
    Co-authored-by: Patryk Makosa <Mentos2509@users.noreply.github.com>

--- 3MCJ/src/Components/SellersList.tsx
+++ 3MCJ/src/Components/SellersList.tsx
@@ -6,40 +6,44 @@ import { globalContext } from "../Context/Context";
 
 function SellersList() {
   const [sellers, setSellers] = useState<any[]>([]);
+  const [filteredSellers, setFilteredSellers] = useState<any[]>([]);
   const { searchingCategory, searchingLocation } = useContext(globalContext);
 
   const fetchSellers = async () => {
-    const q = collection(firebaseDb, "Sellers");
-
-    }
+    const q = query(collection(firebaseDb, "Sellers"));    
     try {
       const sellersSnapshot = await getDocs(q);
       const fetchedSellers: any[] = [];
       sellersSnapshot.forEach((seller) => {
         fetchedSellers.push({ id: seller.id, ...seller.data() });
       });
+      console.log("fetchedSellers", fetchedSellers);
       setSellers(fetchedSellers);
     } catch (error) {
       console.error("Error fetching sellers: ", error);
     }
- 
-  console.log(sellers)
+  }
+
   useEffect(() => {
     fetchSellers();
   }, []);
 
-
-  const filterLocation = () => {
-    
-    if (searchingLocation !== "Wybierz miasto" ) {
-      sellers.filter((element)=> element.city === searchingLocation)
+  const filterSellers = () => {
+  const filteredSellers = sellers.filter((seller) => {   
+    return  ( seller.city === searchingLocation) && (seller.category === searchingCategory)
     }
+  );
+  setFilteredSellers(filteredSellers); 
   }
+
+  useEffect(() => {    
+    filterSellers(); 
+  }, [searchingCategory, searchingLocation, sellers])
 
 
   return (
     <div>
-      {sellers.map((seller) => (
+      {filteredSellers.map((seller) => (
         <div key={seller.id}>
           <h2>{seller.id}</h2>
           <p>{seller.sellerPhoto}</p>
@@ -49,55 +53,6 @@ function SellersList() {
     </div>
   );
 }
-// function SellersList() {
-//   const [products, setProducts] = useState<any[]>([]);
-//   const { searchingCategory, searchingLocation } = useContext(globalContext);
 
-//   const fetchSellers = async () => {
-//     const q = query(collection(firebaseDb, "Sellers"));
-//   };
-
-//   const fetchProducts = async (
-//     searchingCategory: string,
-//     searchingLocation: string
-//   ) => {
-//     const q = query(
-//       collection(firebaseDb, "Sellers")
-//       // where("City", "==", searchingLocation)
-//     );
-//     try {
-//       const productsSnapshot = await getDocs(q);
-//       console.log(productsSnapshot);
-//       const fetchedProducts: any[] = [];
-//       productsSnapshot.forEach((product) => {
-//         fetchedProducts.push({ id: product.id, ...product.data() });
-//       });
-//       console.log(fetchedProducts);
-//       setProducts(
-//         fetchedProducts.filter(
-//           (element) => element.Category === searchingCategory
-//         )
-//       );
-//     } catch (error) {
-//       console.error("Error fetching products: ", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchProducts(searchingCategory, searchingLocation);
-//   }, [searchingCategory, searchingLocation]);
-
-//   return (
-//     <div>
-//       {products.map((product) => (
-//         <div key={product.id}>
-//           <h2>{product.id}</h2>
-//           <p>{product.sellerPhoto}</p>
-//           <p>{product.sellerDescription}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
 
 export default SellersList;
