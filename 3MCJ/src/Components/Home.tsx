1f49e3c54569382c19b2925d33073f88ec66f369
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-02-09 11:51:29 +0100

    Add useState for location and category , prod. map Co-authored-by: Katarzyna Sęk <Kasiatko@users.noreply.github.com> Co-authored-by: Rafał Konieczny <RafalKonieczny@users.noreply.github.com> Co-authored-by: Agnieszka Szczepańska <agnieszka-szczepanska@users.noreply.github.com>

--- 3MCJ/src/Components/Home.tsx
+++ 3MCJ/src/Components/Home.tsx
@@ -5,45 +5,65 @@ import { signOut } from "firebase/auth";
 import { firebaseAuth } from "../main";
 
 function Home() {
-  
   const navigate = useNavigate();
-  const { setIsLogged, isLogged } = useContext(globalContext);
+  const { setIsLogged, isLogged, searchingLocation, setSearchingLocation, searchingCategory, setSearchingCategory } = useContext(globalContext);
 
-  const handleLogInOut = () => {
-    if (!isLogged){
-      navigate('/login')
-    setIsLogged(!isLogged)
+  const handleLogInOut = () => { 
+    if (!isLogged) {
+      navigate("/login");
+      setIsLogged(!isLogged);
     } else {
-      navigate('/')
-      setIsLogged(!isLogged)
-      signOut(firebaseAuth)
-    }  
-  }
+      navigate("/");
+      setIsLogged(!isLogged);
+      signOut(firebaseAuth);
+    }
+  };
 
   const handleRegistration = () => {
-    navigate('/registration')
-  }
+    navigate("/registration");
+  };
 
+  const handleSubmit = (e:React.FormEvent<EventTarget>) => {
+    e.preventDefault();
+    setSearchingLocation(e.target.location.value);
+    setSearchingCategory(e.target.products.value)
+    
+  }
+  console.log(searchingLocation )
+  console.log(searchingCategory)
   return (
     <div>
       <div>
-        <button onClick={handleLogInOut}>{isLogged? "Wyloguj się": "Zaloguj się"}</button> 
+        <button onClick={handleLogInOut}>
+          {isLogged ? "Wyloguj się" : "Zaloguj się"}
+        </button>
         <button onClick={handleRegistration}>Zarejestruj się</button>
       </div>
+      <form onSubmit={(e)=>handleSubmit(e)}>
       <label htmlFor="location">Lokalizacja:</label>
       <select name="location" id="location">
+        <option disabled>Wybierz miasto</option>
         <option value="Gdynia">Gdynia</option>
         <option value="Sopot">Sopot</option>
         <option value="Gdańsk">Gdańsk</option>
       </select>
       <label htmlFor="products">Kategoria produktu:</label>
       <select name="products" id="products">
-        <option value="Wyroby-Miesne">Wyroby Mięsne</option>
-        <option value="Wyroby-Rybne">Wyroby Rybne</option>
-        <option value="nabiał">Nabiał</option>
-        <option value="miód">Miód</option>
-        <option value="Wyroby Alkoholowe">Wyroby Alkoholowe</option>
+        <option value="Wyroby Mięsne">Wyroby Mięsne</option>
+        <option value="Wyroby Rybne">Wyroby Rybne</option>
+        <option value="Wyroby Cukiernicze">Wyroby Cukiernicze</option>
+        <option value="Nabiał">Nabiał</option>
+        <option value="Wyroby Garmażeryjne">Wyroby Garmażeryjne</option>
       </select>
+      <input type="submit" value={"Znajdź pyszności"} />
+      </form>
+      <div>
+        <div>Wyroby Mięsne</div>
+        <div>Wyroby Rybne</div>
+        <div>Wyroby Cuiernicze</div>
+        <div>Nabiał</div>
+        <div>Wyroby Garmażeryjne</div>
+      </div>
     </div>
   );
 }
