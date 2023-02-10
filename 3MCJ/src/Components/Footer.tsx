da938d07542f5ea1af67e44bb6e077533f1fd603
Gabriel Dąbrowski <dabgabriel@gmail.com>
2023-02-10 14:06:13 +0100

    Footer structure complete

--- 3MCJ/src/Components/Footer.tsx
+++ 3MCJ/src/Components/Footer.tsx
@@ -39,8 +39,8 @@ function Footer() {
             <ul className="site-authors">
                 {authors.map(autor => {
                     return <li className="author">
-                    <a className="author-link" href={autor.autorGithubLink}>{autor.autorName}
-                    </a>
+                    <a className="author-link" href={autor.autorGithubLink} target="_blank">{autor.autorName}
+</a>
                 </li>
 
                 }) }
