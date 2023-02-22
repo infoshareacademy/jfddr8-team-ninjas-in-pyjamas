e63eed65236a4faa5daad54de2de95bd8668c65f
Rafał Konieczny <rkonieczny87@gmail.com>
2023-02-22 13:26:09 +0100

    config GH pages

--- 3MCJ/vite.config.ts
+++ 3MCJ/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+  base:"/jfddr8-team-ninjas-in-pyjamas/"
 })
+
